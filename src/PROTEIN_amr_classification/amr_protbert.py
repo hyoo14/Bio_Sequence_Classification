466a246462d75f10975cb51a31bfc23858ea8e54
hyoo14 <hyoo0014@naver.com>
2025-04-01 01:43:44 -0400

    Update amr_protbert.py

--- src/PROTEIN_amr_classification/amr_protbert.py
+++ src/PROTEIN_amr_classification/amr_protbert.py
@@ -67,27 +67,118 @@ tokenized_train = dataset_train.map(tokenize_function, batched=True, remove_colu
 tokenized_valid = dataset_valid.map(tokenize_function, batched=True, remove_columns=["data"])
 tokenized_test  = dataset_test.map(tokenize_function, batched=True, remove_columns=["data"])
 
-# Apply PEFT
-peft_config = LoraConfig(
-    task_type=TaskType.SEQ_CLS,
-    inference_mode=False,
-    r=1,
-    lora_alpha=32,
-    lora_dropout=0.1,
-    target_modules=["query", "value"]
+
+#### v1: LoRA peft model
+# # Apply PEFT
+# peft_config = LoraConfig(
+#     task_type=TaskType.SEQ_CLS,
+#     inference_mode=False,
+#     r=1,
+#     lora_alpha=32,
+#     lora_dropout=0.1,
+#     target_modules=["query", "value"]
+# )
+# model = get_peft_model(model, peft_config)
+# model.to(device)
+
+
+# v2: focal loss one layer finetune(much much better performance)
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
+
+class_weights = compute_class_weight(
+    class_weight="balanced",
+    classes=np.unique(train_df["label"]),
+    y=train_df["label"]
 )
-model = get_peft_model(model, peft_config)
-model.to(device)
+class_weights_tensor = torch.tensor(class_weights, dtype=torch.float).to(device)
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2.0):
+        super().__init__()
+        self.gamma = gamma
+        self.ce = nn.CrossEntropyLoss(reduction='none')
+
+    def forward(self, input, target):
+        ce_loss = self.ce(input, target)
+        pt = torch.exp(-ce_loss)
+        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
+        return focal_loss
+
+from transformers import BertPreTrainedModel, BertModel
+import torch.nn as nn
+from transformers.modeling_outputs import SequenceClassifierOutput
+
+class CustomProtBertForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config, class_weights):
+        super().__init__(config)
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.class_weights = class_weights
+        #self.loss_fct = nn.CrossEntropyLoss(weight=self.class_weights)
+        self.loss_fct = FocalLoss(gamma=2.0)
+
+
+        self.init_weights()
+
+    def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        
+
+        loss = None
+        if labels is not None:
+            loss = self.loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+from transformers import AutoConfig
+
+# Config 불러오기
+config = AutoConfig.from_pretrained(model_name, num_labels=len(label_to_id))
+
+# Class weight 반영된 커스텀 모델 로드 (LoRA X)
+model = CustomProtBertForSequenceClassification.from_pretrained(
+    model_name, config=config, class_weights=class_weights_tensor
+).to(device)
+
+
+
+
+
+
 
 # Training
+# training_args = TrainingArguments(
+#     output_dir="/content/protbert_output",
+#     evaluation_strategy="epoch",
+#     save_strategy="epoch",
+#     learning_rate=5e-4,
+#     per_device_train_batch_size=8,
+#     per_device_eval_batch_size=64,
+#     num_train_epochs=2,
+#     weight_decay=0.01,
+#     load_best_model_at_end=True,
+#     metric_for_best_model="f1_macro",
+#     logging_dir="/content/logs"
+# )
 training_args = TrainingArguments(
     output_dir="/content/protbert_output",
     evaluation_strategy="epoch",
     save_strategy="epoch",
-    learning_rate=5e-4,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=64,
-    num_train_epochs=2,
+    learning_rate=2e-5,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=32,
+    num_train_epochs=5,
     weight_decay=0.01,
     load_best_model_at_end=True,
     metric_for_best_model="f1_macro",
